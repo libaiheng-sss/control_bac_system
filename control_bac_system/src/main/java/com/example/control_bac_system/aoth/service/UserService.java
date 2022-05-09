5f1ff9e980c783314239fe75abb22273c0e5447b
libaiheng <1397183579@qq.com>
2022-05-09 17:30:27 +0800

    jwt获取token

--- control_bac_system/src/main/java/com/example/control_bac_system/aoth/service/UserService.java
+++ control_bac_system/src/main/java/com/example/control_bac_system/aoth/service/UserService.java
@@ -1,25 +1,40 @@
 package com.example.control_bac_system.aoth.service;
 
+import cn.hutool.core.util.StrUtil;
 import com.example.control_bac_system.aoth.entity.User;
+import com.example.control_bac_system.entity.UserInfo;
+import com.example.control_bac_system.mapper.UserMapper;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 
 @Service
 public class UserService implements UserDetailsService {
 
+
     @Resource
     private PasswordEncoder passwordEncoder;
+    @Resource
+    private UserMapper userMapper;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        // todo 查询数据库
-        User user = new User("admin",passwordEncoder.encode("123"), AuthorityUtils.commaSeparatedStringToAuthorityList("admin,normal"));
+        if (StrUtil.isNotBlank(username)){
+            throw new UsernameNotFoundException("用户名不存在！");
+        }
+        UserInfo userInfo = userMapper.selectUserByUsername(username);
+        if (null == userInfo){
+            throw new UsernameNotFoundException("用户名不存在！");
+        }
+
+        User user = new User("admin",passwordEncoder.encode("admin"), AuthorityUtils.commaSeparatedStringToAuthorityList("admin,normal"));
         return user ;
     }
+
 }
--- control_bac_system/src/main/java/com/example/control_bac_system/service/LoginServiceImpl.java
+++ control_bac_system/src/main/java/com/example/control_bac_system/service/LoginServiceImpl.java
@@ -1,12 +1,19 @@
 package com.example.control_bac_system.service;
 
+import com.example.control_bac_system.entity.UserInfo;
+import com.example.control_bac_system.mapper.UserMapper;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class LoginServiceImpl implements LoginService{
 
+    @Resource
+    private UserMapper userMapper;
     @Override
     public String login(String username, String password) {
-        return null;
+        UserInfo user = userMapper.selectUserByUsername("w");
+        return user.toString();
     }
 }
